c49aa22269b4d13097e0559d7ca84243a34fa440
toni moragues <tmoragues@gmail.com>
2020-10-21 07:30:32 +0200

    add persones

---
+++ persones.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+$servername = "localhost";
+$username = "admin";
+$password = "admin";
+$dbname = "proves";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, nom, llinatges FROM persones";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["nom"]. " " . $row["llinatges"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+</body>
+</html>
