ef1181e98e73e93b9cd05d9d26e393b4589a5546
toni moragues <tmoragues@gmail.com>
2020-10-26 09:26:29 +0100

    add form

---
+++ form.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <form action="mostra-form.php" method="get">
+          <div class="form-group">
+            <label for="nom">Nom</label>
+            <input type="text" class="form-control" id="nom" aria-describedby="nomHelp" placeholder="el vostre nom">
+          </div>
+          <div class="form-group">
+            <label for="cognom">Nom</label>
+            <input type="text" class="form-control" id="cognom" aria-describedby="emailHelp" placeholder="el vostre cognom">
+          </div>
+          <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
