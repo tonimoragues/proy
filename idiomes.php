279001250c3a83cc024eedede1f1e2bb21c27282
toni moragues <tmoragues@gmail.com>
2020-10-26 07:50:06 +0100

    fix 1

---
+++ idiomes.php
@@ -0,0 +1,35 @@
+<?php
+$headers=getallheaders();
+//print($headers["User-Agent"]);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body>
+
+<h1>
+  <?php
+  $lang="ca";
+  switch ($lang) {
+      case "ca":
+          echo "Benvinguts a la meva pàgina web";
+          break;
+      case "es":
+          echo "Bienvendios a mi página web";
+          break;
+      case "en":
+          echo "Wellcome to my web page";
+          break;
+  }
+  ?>
+</h1>
+</body>
+</html>
